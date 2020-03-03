e7e07e0014c5682f282b50352bf4cbf8148269af
ArlanchikDrey <arlanchikdrey@yandex.ru>
2020-03-03 10:51:45 +0300

    start dart tour

---
+++ lib/dart_tour/main.dart
@@ -0,0 +1,6 @@
+import 'package:ones/dart_tour/types.dart';
+
+void main(){
+  String age = parseIntToString(150);
+  print(age);
+}
---
+++ lib/dart_tour/types.dart
@@ -0,0 +1,11 @@
+String parseIntToString(int num){
+  return num.toString();
+}
+
+int parseStringToInt(String age){
+  return int.parse(age);
+}
+
+String parseDoubleToString(double num){
+  return num.toStringAsFixed(2);
+}
